399f8d99d3606b6317c5b686dea7b300cb7f4578
Jeremiah <huertajeremiah@gmail.com>
2020-05-10 21:29:40 -0700

    push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Chesseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Pastrami Burger');
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+
+
+var PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+var hbs = require('express-handlebars');
+
+app.engine("handlebars", hbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var router = require("./controllers/burgers_controller.js");
+
+app.use(router);
+
+app.listen(PORT,function(){
+    console.log("Server listening on: http://localhost:" + PORT);
+});
